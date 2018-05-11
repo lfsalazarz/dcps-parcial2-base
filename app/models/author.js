9253375022c1d582eaef318913e247700c837333
lfsalazarz <lfsalazarz@unal.edu.co>
2018-05-11 10:26:30 -0500

    Se define el modelo Author

---
+++ app/models/author.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    username: DS.attr('string'),
+    email: DS.attr('string'),
+    profilePicture: DS.attr('string'),
+
+    //blogPost: DS.hasMany('blogPost')
+});
